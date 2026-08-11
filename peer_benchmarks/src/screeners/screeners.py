5d5cd8edc715179b35c751593f82ebaedcfb721a
Justin <justinw86420@gmail.com>
2026-08-10 20:18:32 -0400

    Write screener for obtaining stocks in various industries

--- peer_benchmarks/src/screeners/screeners.py
+++ peer_benchmarks/src/screeners/screeners.py
@@ -1,5 +1,12 @@
 import yfinance as yf
 
 # Screener for getting industries
-def screen_by_industry():
-    yf.EquityQuery('and')
+def screen_by_industry(industry):
+    query = yf.EquityQuery('and', [
+        yf.EquityQuery('is-in', ['industry', f'{industry}']),
+        yf.EquityQuery('is-in', ['exchange', 'NYQ', 'NMS', 'ASE', 'NCM']),
+    ])
+    query_results = yf.screen(query, size=250)
+    data = query_results['quotes']
+    stocks = [d['symbol'] for d in data]
+    return stocks
