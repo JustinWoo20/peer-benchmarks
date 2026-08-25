b9799579f602ffecae6942c7c1fefe3f31f3a028
Justin <justinw86420@gmail.com>
2026-08-25 19:44:34 -0400

    Clean ind keys that do not have any values

--- peer_benchmarks/src/industry/industry_avg.py
+++ peer_benchmarks/src/industry/industry_avg.py
@@ -29,7 +29,7 @@ def get_market_cap(ticker):
         shares_outstanding = info.get('sharesOutstanding') or info.get('floatShares')
         current_price = info.get('currentPrice')
         if current_price is None or shares_outstanding is None:
-            return 0
+            return None
         market_cap = shares_outstanding * current_price
     return market_cap
 
@@ -37,8 +37,14 @@ def get_income_statistics(ticker):
     income_statement = ticker.get_income_stmt()
     income_transposed = income_statement.transpose()
     revenue = income_transposed.get('TotalRevenue')
-    recent_revenue = revenue.iloc[0] if not None else 0
-    previous_revenue_in = revenue.iloc[1] if not None else 0
+    if revenue is None:
+        return None, None, None
+    elif len(revenue) < 2:
+        return None, None, None
+    else:
+        recent_revenue = revenue.iloc[0]
+        previous_revenue_in = revenue.iloc[1]
+
     gp = income_transposed.get('GrossProfit')
     if gp is None:
         return recent_revenue, previous_revenue_in, 0
@@ -78,15 +84,18 @@ def pe_ratios(ticker):
 # Obtain companies in each industry in major American stock exchanges
 screener_industries = web_scraper.obtain_equity_query()
 industry_stock_dict = {}
+delete_list = []
 for si in screener_industries.values():
     for i in si:
         stock_list = screen_by_industry(industry=i)
-        industry_stock_dict[i] = stock_list
-
+        if stock_list is not None:
+            industry_stock_dict[i] = stock_list
 
 industry_values = {}
 for ind, stocks in industry_stock_dict.items():
     print(f'Now working on {ind}')
+    if stocks is None:
+        del
     industry_mc = 0
     industry_equity = 0
     industry_debt = 0
@@ -102,18 +111,22 @@ for ind, stocks in industry_stock_dict.items():
         yf_ticker = get_yf_ticker(s)
         # Find total market cap
         mc = get_market_cap(yf_ticker)
+        if mc is None:
+            continue
         industry_mc += mc
+        # Find most recent revenue and previous
+        total_revenue, previous_revenue, gross_profit = get_income_statistics(yf_ticker)
+        if total_revenue is None:
+            continue
+        industry_revenue += total_revenue
+        industry_previous_revenue += previous_revenue
+        # Gross Profit
+        industry_gross_profit += gross_profit
         # Find most recent stockholder's equity
         equity, total_debt = get_balance_statistics(yf_ticker)
         industry_equity += equity
         # Find total debt per industry
         industry_debt += total_debt
-        # Find most recent revenue and previous
-        total_revenue, previous_revenue, gross_profit = get_income_statistics(yf_ticker)
-        industry_revenue += total_revenue
-        industry_previous_revenue += previous_revenue
-        # Gross Profit
-        industry_gross_profit += gross_profit
         # Trailing P/E and Forward P/E
         pe_market_cap, pe_net_income, forward_pe = pe_ratios(yf_ticker)
         industry_pe_mc += pe_market_cap
@@ -143,50 +156,3 @@ for ind, stocks in industry_stock_dict.items():
     industry_values[ind] = new_row
 
 print(industry_values)
-        # Find company
-#         weighted_pb = market_cap * pb
-#         weighted_pb_list.append(weighted_pb)
-#         weighted_de = market_cap * de
-#         weighted_de_list.append(weighted_de)
-#         weighted_rg = market_cap * revenue_growth
-#         weighted_rg_list.append(weighted_rg)
-#         weighted_gm = market_cap * gross_margin
-#         weighted_gm_list.append(weighted_gm)
-#         weighted_ttmpe = market_cap * trailing_pe
-#         weighted_ttmpe_list.append(weighted_ttmpe)
-#         weighted_fpe = market_cap * forward_pe
-#         weighted_fpe_list.append(weighted_fpe)
-#         industry_size += market_cap
-#
-#     # Add up industry totals
-#     industry_pb_sum = sum(weighted_pb_list)
-#     industry_de_sum = sum(weighted_de_list)
-#     industry_rg_sum = sum(weighted_rg_list)
-#     industry_gm_sum = sum(weighted_gm_list)
-#     industry_ttmpe_sum = sum(weighted_ttmpe_list)
-#     industry_fpe_sum = sum(weighted_fpe_list)
-#
-#     industry_pb = industry_pb_sum / industry_size
-#     industry_de = industry_de_sum / industry_size
-#     industry_rg = industry_rg_sum / industry_size
-#     industry_gm = industry_gm_sum / industry_size
-#     industry_ttmpe = industry_ttmpe_sum / industry_size
-#     industry_fpe = industry_fpe_sum / industry_size
-#
-#     inner_dict = {'P/B': industry_pb,
-#                   'D/E': industry_de,
-#                   'Revenue Growth': industry_rg,
-#                   'Gross Profit Margin': industry_gm,
-#                   'Trailing PE': industry_ttmpe,
-#                   'Forward PE': industry_fpe}
-#     print(inner_dict)
-#
-#     industry_values[ind] = inner_dict
-#
-# print(industry_values)
-
-
-# new_row = [(i,) for i in industry_list]
-# cur.executemany("INSERT INTO industry_averages VALUES(industries)", new_row)
-# conn.close()
-#
