3b3d3716c48ff578de2d24440594d762b684017c
Justin <justinw86420@gmail.com>
2026-09-24 10:56:14 -0400

    Reorganize formulas for easier use when adding more benchmarks in the future

--- peer_benchmarks/src/industry/financial_services/financial.py
+++ peer_benchmarks/src/industry/financial_services/financial.py
@@ -2,14 +2,14 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 
-def get_yf_ticker(ticker):
-    return yf.Ticker(ticker)
+def get_yf_ticker(stock_symbol):
+    return yf.Ticker(stock_symbol)
 
 def safe_transpose(df):
     # Returns a transposed dataframe or none if empty
     if df is None or df.empty:
         return None
-    return df.transpose().fillna(0)
+    return df.transpose()
 
 def get_financial_statements(yf_ticker):
     # Obtain the most recent financial statements for a company
@@ -22,6 +22,7 @@ def get_financial_statements(yf_ticker):
     info = yf_ticker.info
     return income_t, balance_t, cash_t, info
 
+# ------------------------------------Retrieve values from financial statements-----------------------------------------
 def get_market_cap(ticker_info):
     # Returns a company's market cap
     market_cap = ticker_info.get('marketCap') or ticker_info.get('nonDilutedMarketCap')
@@ -29,156 +30,241 @@ def get_market_cap(ticker_info):
         shares_outstanding = ticker_info.get('sharesOutstanding') or ticker_info.get('floatShares')
         current_price = ticker_info.get('currentPrice')
         if current_price is None or shares_outstanding is None:
-            return 0
+            return None
         market_cap = shares_outstanding * current_price
     return market_cap
 
-def get_shareholder_equity(balance_sheet):
-    # Returns the most recent shareholder equity value from the balance sheet
+def get_equity(balance_sheet):
+    # Obtain shareholder equity, returns none if not available
     if balance_sheet is None:
-        return 0
-    total_shareholder_equity_series = balance_sheet.get('StockholdersEquity')
-    if total_shareholder_equity_series is None:
-        return 0
-    elif total_shareholder_equity_series.iloc[0] <= 0:
-        return 0
+        return None
+    if 'StockholdersEquity' in balance_sheet.columns:
+        shareholder_equity_series = balance_sheet.get('StockholdersEquity')
+    elif 'ShareholdersEquity' in balance_sheet.columns:
+        shareholder_equity_series = balance_sheet.get('ShareholdersEquity')
     else:
-        total_shareholder_equity = total_shareholder_equity_series.iloc[0]
-        return total_shareholder_equity
+        return None
+
+    shareholder_equity = shareholder_equity_series.iloc[0]
+    if shareholder_equity < 0:
+        return None
+
+    return shareholder_equity
 
 def get_total_debt(balance_sheet):
-    # Returns the most recent total debt value from the balance sheet
+    # Retrieves total debt from a company's balance sheet and returns None if not available
     if balance_sheet is None:
-        return 0
-    td = balance_sheet.get('TotalDebt')
-    if td is None:
-        return 0
+        return None
+    if 'TotalDebt' in balance_sheet.columns:
+        total_debt_series = balance_sheet.get('TotalDebt')
     else:
-        td = td.iloc[0]
-        return td
+        return None
+    total_debt = total_debt_series.iloc[0]
+    return total_debt
 
 def get_revenue(income_statement):
-    # Returns the most recent year's revenue and the previous year's
+    # Retrieves most recent year's revenue and previous year's
     if income_statement is None:
-        return 0, 0
-    revenue = income_statement.get('TotalRevenue')
-    if revenue is None:
-        return 0, 0
-    elif len(revenue) < 2:
-        return 0, 0
-    recent_revenue = revenue.iloc[0]
-    previous_revenue = revenue.iloc[1]
-    if recent_revenue == 0 or previous_revenue == 0:
-        return 0, 0
+        return None
+    if 'TotalRevenue' in income_statement.columns:
+        revenue_series = income_statement.get('TotalRevenue')
+    elif 'Revenue' in income_statement.columns:
+        revenue_series = income_statement.get('Revenue')
     else:
-        return recent_revenue, previous_revenue
+        return None
 
-def get_pe_ratios(income_statement, t_info):
-    # Return market cap, net income, and forward p/e estimate
+    if len(revenue_series) < 2:
+        return None
+
+    recent_revenue = revenue_series.iloc[0]
+    previous_revenue = revenue_series.iloc[1]
+    return recent_revenue, previous_revenue
+
+def get_gross_profit(income_statement):
     if income_statement is None:
-        return 0, 0, 0
+        return None
+    if 'GrossProfit' in income_statement.columns:
+        gross_profit_series = income_statement.get('GrossProfit')
+    else:
+        return None
 
-    net_income = None
-    for col in ('NetIncome', 'NetIncomeCommonStockholders', 'NetIncomeToCommon'):
-        if col in income_statement.columns:
-            net_income = income_statement[col].iloc[0]
-            break
+    gross_profit = gross_profit_series.iloc[0]
+    return gross_profit
 
-    if net_income is None or net_income == 0:
-        return 0, 0, 0
+def get_net_income(income_statement):
+    if income_statement is None:
+        return None
+    if 'NetIncome' in income_statement.columns:
+        net_income_series = income_statement.get('NetIncome')
+    elif 'NetIncomeCommonStockholders' in income_statement.columns:
+        net_income_series = income_statement.get('NetIncomeCommonStockholders')
+    elif 'NetIncomeToCommon' in income_statement.columns:
+        net_income_series = income_statement.get('NetIncomeToCommon')
+    else:
+        return None
 
-    market_cap = get_market_cap(t_info)
-    ttm_pe = market_cap / net_income
-    f_pe = t_info.get('forwardPE')
+    net_income = net_income_series.iloc[0]
+    return net_income
+
+#----------------------------------------------------------------------------------------------------------------------
+
+#----------------------------------------------Match metrics to ensure accurate computation----------------------------
+def get_pb_ratio_inputs(balance_sheet, stock_info):
+    # industry market cap / industry equity
+    com_market_cap = get_market_cap(stock_info)
+    com_equity = get_equity(balance_sheet)
+
+    if com_market_cap is None or com_equity is None:
+        return None
+    pairs = (com_market_cap, com_equity)
+    return pairs
+
+def calculate_pb_ratio(mc_equity_pairs):
+    # industry market cap / industry equity
+    industry_mc_sum = sum(mc for mc, _ in mc_equity_pairs)
+    industry_equity_sum = sum(equity for _, equity in mc_equity_pairs)
+
+    return round(industry_mc_sum / industry_equity_sum, 2)
+
+def get_de_ratio_inputs(balance_sheet,):
+    # Industry total debt / industry equity
+    com_debt = get_total_debt(balance_sheet=balance_sheet)
+    com_equity = get_equity(balance_sheet=balance_sheet)
+    pairs = (com_debt, com_equity)
+    return pairs
+
+def calculate_de_ratio(debt_equity_pairs):
+    # industry total debt / industry equity
+    industry_debt_sum = sum(debt for debt, _ in debt_equity_pairs)
+    industry_equity = sum(equity for _, equity in debt_equity_pairs)
+    return round(industry_debt_sum / industry_equity, 2)
+
+def get_rev_growth_inputs(income_statement):
+    # Recent / Previous - 1
+    revenue, previous = get_revenue(income_statement)
+    pairs = (revenue, previous)
+    return pairs
+
+def calculate_rev_growth(revenue_pairs):
+    current_total_revenue = sum(current_r for current_r, _ in revenue_pairs)
+    previous_revenue_sum = sum(previous for _, previous in revenue_pairs)
+    return round(current_total_revenue / previous_revenue_sum - 1, 2)
+
+def get_gross_margin_inputs(income_statement):
+    # Industry gross profit / industry revenue
+    revenue, previous = get_revenue(income_statement=income_statement)
+    com_gross_profit = get_gross_profit(income_statement=income_statement)
+    pairs = (com_gross_profit, revenue)
+    return pairs
+
+def calculate_gross_margin(revenue_margin_pairs):
+    gross_profit_sum = sum(gp for gp, _ in revenue_margin_pairs)
+    revenue_sum = sum(revenue for _, revenue in revenue_margin_pairs)
+
+    return round(gross_profit_sum / revenue_sum, 2)
+
+
+def get_roe_inputs(income_statement, balance_sheet):
+    # Industry net income / industry equity
+    com_net_income = get_net_income(income_statement=income_statement)
+    com_equity = get_equity(balance_sheet=balance_sheet)
+    pairs = (com_net_income, com_equity)
+    return pairs
+
+def calculate_roe(roe_pairs):
+    # Industry net income / industry equity
+    net_income_sum = sum(ni for ni, _ in roe_tuple)
+    equity_sum = sum(equity for equity, _ in roe_tuple)
+
+    return round(net_income_sum / equity_sum, 2)
+
+def get_ttm_pe_inputs(ticker_info, income_statement):
+    # market_cap / net_income
+    com_market_cap = get_market_cap(ticker_info=ticker_info)
+    com_net_income = get_net_income(income_statement=income_statement)
+    pairs = (com_market_cap, com_net_income)
+    return pairs
+
+def calculate_ttm_pe(mc_ni_pairs):
+    # industry market cap / industry net income
+    industry_market_cap = sum(mc for mc, _ in mc_ni_pairs)
+    industry_net_income = sum(ni for _, ni in mc_ni_pairs)
+    return round(industry_market_cap / industry_net_income, 2)
+
+def get_forward_pe(ticker_info):
+    f_pe_list = []
+    if ticker_info is None:
+        return None
+    f_pe = ticker_info.get('forwardPE')
     if f_pe is None:
-        f_pe = 0
+        return None
     elif f_pe == 'Infinity':
-        f_pe = 0
+        return None
+    f_pe_list.append(f_pe)
+    return f_pe_list
 
-    if ttm_pe <= 0:
-        return 0, 0, f_pe
-    else:
-        return market_cap, net_income, f_pe
+def calculate_forward_pe(industry_pe_list):
+    # Take median of forward PE list
+    forward_pe_cleaned = [x for x in industry_pe_list if x]
+    median_forward_pe = np.median(forward_pe_cleaned)
+    return median_forward_pe
 
-def calculate_pb(market_cap, equity):
-    return round(market_cap / equity,2)
+#---------------------------------------------------------------------------------------------------------------------
 
-def calculate_de(debt, equity):
-    return round(debt / equity, 2)
+# -------------------------------Calculate benchmarks function--------------------------------------------------------
+# This is the main function that will be called when calculating industry averages
 
-def calculate_revenue_growth(revenue, previous):
-    revenue_growth = (revenue / previous) - 1
-    return round(revenue_growth, 2)
-
-def calculate_roe(revenue, equity):
-    return round(revenue / equity, 2)
-
-def calculate_ttm_pe(market_cap, net_income):
-    if net_income == 0:
-        return 0
-    else:
-        ttm_pe = market_cap / net_income
-        return round(ttm_pe, 2)
-
-def calculate_forward_pe(f_pe_list):
-    # Clean forward pe list and find the median
-    forward_pe_cleaned = [x for x in f_pe_list if x != 0]
-    median_pe = np.median(forward_pe_cleaned)
-    return median_pe
-
-def calculate_f_benchmarks(ind_stock_dict):
+def calculate_benchmarks(sect_ind_stock_dict):
     industry_values = {}
-    for ind, stocks in ind_stock_dict.items():
-        # Running totals
-        industry_market_cap = 0
-        industry_shareholder_equity = 0
-        industry_total_debt = 0
-        industry_recent_revenue = 0
-        industry_previous_revenue = 0
-        industry_pe_market_cap = 0
-        industry_net_income = 0
-        industry_f_pe = []
+    for sector, industries in sect_ind_stock_dict.items():
+        for ind, stock_list in industries.items():
+            # Blank list for storing inputs
+            pb_list = []
+            de_list = []
+            revenue_growth_list = []
+            gross_margin_list = []
+            roe_list = []
+            ttm_pe_list = []
+            forward_pe_list = []
+            for stock in stock_list:
+                yf_ticker = get_yf_ticker(stock)
+                income, balance, cash, stock_info = get_financial_statements(yf_ticker=yf_ticker)
+                # Append each tuple to a list
+                pb_ratio_inputs = get_pb_ratio_inputs(balance_sheet=balance, stock_info=stock_info)
+                pb_list.append(pb_ratio_inputs)
+                de_ratio_inputs = get_de_ratio_inputs(balance_sheet=balance)
+                de_list.append(de_ratio_inputs)
+                revenue_growth_inputs = get_rev_growth_inputs(income_statement=income)
+                revenue_growth_list.append(revenue_growth_inputs)
+                gross_margin_inputs = get_gross_margin_inputs(income_statement=income)
+                gross_margin_list.append(gross_margin_inputs)
+                roe_inputs = get_roe_inputs(income_statement=income, balance_sheet=balance)
+                roe_list.append(roe_inputs)
+                ttm_pe_inputs = get_ttm_pe_inputs(ticker_info=yf_ticker, income_statement=income)
+                ttm_pe_list.append(ttm_pe_inputs)
+                forward_pe_inputs = get_forward_pe(ticker_info=stock_info)
+                forward_pe_list.append(forward_pe_inputs)
+            # Calculate metrics
+            pb_industry = calculate_pb_ratio(mc_equity_pairs=pb_list)
+            de_industry = calculate_de_ratio(debt_equity_pairs=de_list)
+            revenue_growth_industry = calculate_rev_growth(revenue_pairs=revenue_growth_list)
+            gross_margin_industry = calculate_gross_margin(revenue_margin_pairs=gross_margin_list)
+            roe_industry = calculate_roe(roe_pairs=roe_list)
+            ttm_pe_industry = calculate_ttm_pe(mc_ni_pairs=ttm_pe_list)
+            forward_pe_industry = calculate_forward_pe(industry_pe_list=forward_pe_list)
 
-        for s in stocks:
-            # Retrieve values from financial statements
-            t = get_yf_ticker(s)
-            income_s, balance_s, cash_f, stock_info = get_financial_statements(t)
-            # Market cap
-            com_market_cap = get_market_cap(ticker_info=stock_info)
-            industry_market_cap += com_market_cap
-            # Shareholder's equity for P/B and RoE
-            com_shareholder_equity = get_shareholder_equity(balance_sheet=balance_s)
-            industry_shareholder_equity += com_shareholder_equity
-            # Total debt for debt to equity ratio
-            com_total_debt = get_total_debt(balance_sheet=balance_s)
-            industry_total_debt += com_total_debt
-            # Revenue for revenue growth and RoE
-            com_recent_rev, com_previous_revenue = get_revenue(income_statement=income_s)
-            industry_recent_revenue += com_recent_rev
-            industry_previous_revenue += com_previous_revenue
-            # Market cap for pe calculations, net income, and forward P/E estimates
-            com_pe_market_cap, com_net_income, com_f_pe = get_pe_ratios(income_statement=income_s, t_info=stock_info)
-            industry_pe_market_cap += com_pe_market_cap
-            industry_net_income += com_net_income
-            industry_f_pe.append(com_f_pe)
+            new_row = {'Sector': sector,
+                       'Industry': ind,
+                       'PB_Ratio': pb_industry,
+                       'DE_Ratio': de_industry,
+                       'Revenue_Growth': revenue_growth_industry,
+                       'Gross_Margin': gross_margin_industry,
+                       'RoE': roe_industry,
+                       'TTM_PE': ttm_pe_industry,
+                       'Forward_PE': forward_pe_industry,}
+            print(new_row)
 
-        # Calculations
-        industry_pb = calculate_pb(market_cap=industry_market_cap, equity=industry_shareholder_equity)
-        industry_de = calculate_de(debt=industry_total_debt, equity=industry_shareholder_equity)
-        industry_revenue_growth = calculate_revenue_growth(revenue=industry_recent_revenue, previous=industry_previous_revenue)
-        industry_roe = calculate_roe(revenue=industry_recent_revenue, equity=industry_shareholder_equity)
-        industry_ttm_pe = calculate_ttm_pe(market_cap=industry_pe_market_cap, net_income=industry_net_income)
-        industry_forward_pe = calculate_forward_pe(f_pe_list=industry_f_pe)
-
-        new_row = {'pb_ratio': industry_pb,
-                   'de_ratio': industry_de,
-                   'revenue_growth': industry_revenue_growth,
-                   'gross_margin': industry_roe,
-                   'trailingPE': industry_ttm_pe,
-                   'forwardPE': industry_forward_pe,}
-        print(new_row)
-
-        industry_values[ind] = new_row
+            industry_values[ind] = new_row
 
     print(industry_values)
     return industry_values
--- peer_benchmarks/src/industry/industry_avg.py
+++ peer_benchmarks/src/industry/industry_avg.py
@@ -2,32 +2,49 @@ from datetime import date
 import sqlite3 as sql
 from peer_benchmarks.src.web_scraping import web_scraper
 from peer_benchmarks.src.screeners.screeners import screen_by_industry
-from non_financial_services.non_financial import calculate_nf_benchmarks
 from peer_benchmarks.config import PEER_BENCHMARKS
-import yfinance as yf
 
 # Get current year
 current_year = date.today().year
 current_month = date.today().month
 
 # Open SQLite connection
-conn = sql.connect(PEER_BENCHMARKS / f"peer_benchmarks_{current_year}_{current_month}.db")
+# conn = sql.connect(PEER_BENCHMARKS / f"peer_benchmarks_{current_year}_{current_month}.db")
+# cur = conn.cursor()
+# # Drop tables if they exists
+# cur.execute("DROP TABLE IF EXISTS industries")
+# # Create new tables for most recent values
+# cur.execute("""CREATE TABLE industries
+#             (Industry TEXT NOT NULL,
+#             Sector TEXT NOT NULL,
+#             PB_Ratio REAL,
+#             DE_Ratio REAL,
+#             RoE REAL,
+#             Revenue_Growth REAL,
+#             Gross_Margin REAL,
+#             TTM_PE REAL,
+#             Forward_PE)""")
 
-# Obtain companies in each industry in major American stock exchanges
+# Obtain sector and industry dictionary
 industries_scraped = web_scraper.obtain_equity_query()
-financial_services_industries = {}
-non_financial_services_industries = {}
 
-# Split between financial services and nonfinancial services
+# Create dictionary with stocks from industry screening
+# {Sector: {Industry: [], Industry: []}, Sector:}
+screener_results = {}
 for sector, industries in industries_scraped.items():
+    inner_dict = {}
     for i in industries:
         stock_list = screen_by_industry(industry=i)
-        if sector == 'Financial Services' and stock_list is not None:
-            financial_services_industries[i] = stock_list
-        elif stock_list:
-           non_financial_services_industries[i] = stock_list
+        if stock_list:
+            inner_dict[i] = stock_list
+    screener_results[sector] = inner_dict
 
-# Non-financial services industries
-calculate_nf_benchmarks(non_financial_services_industries)
+# Calculate benchmarks
+# calculate_benchmakrs(screener_results)
+# Financial services industries
+# calculate_f_benchmarks(industry_stock_dict=financial_services_industries)
+#
+# # Non-financial services industries
+# calculate_nf_benchmarks(industry_stock_dict=non_financial_services_industries)
 
-conn.close()
+# conn.close()
